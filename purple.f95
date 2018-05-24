1448085f65324e451f81d923785563c505b29c2f
Joshua E. Roybal <30689915+joshroybal@users.noreply.github.com>
2018-05-24 01:23:02 -0500

    Add files via upload
    These files had been replaced accidentally by their analogs in the cgi-bin version.

--- purple.f95
+++ purple.f95
@@ -8,7 +8,7 @@ program purple
          integer, intent(inout), dimension(0:3) :: levcnt
          integer, intent(in) :: fst, mdm, slw
       end subroutine step
-      ! function simulates pegboard mapping of sixes and twenties
+      ! function emulates pegboard mapping of sixes and twenties
       function pegboard(ptch, sixes, twenties, VOWELS, CONSONANTS) result(ctch)
          character, intent(in) :: ptch
          character (len = *), intent(in) :: sixes, twenties, VOWELS, CONSONANTS
@@ -20,7 +20,7 @@ program purple
          integer, intent(in) :: NS, NL, NR, lev, stp
          integer, intent(in), dimension(NS,NL) :: relay
       end function relay_six
-      ! function simulates twenties relay
+      ! function simulate twenties relay
       function relay_twenty(NT, NL, NR, relay, lev, stp)
          integer :: relay_twenty
          integer, intent(in) :: NT, NL, NR
@@ -32,23 +32,20 @@ program purple
    ! variable and array declarations
    character (len = 6), parameter :: VOWELS = 'AEIOUY'
    character (len = 20), parameter :: CONSONANTS = 'BCDFGHJKLMNPQRSTVWXZ'
-   integer, parameter :: NR = 3, NL = 25, NS = 6, NT = 20, LIM = 262144
-   integer :: i, j, n, fst, mdm, slw, idx, recno, eof
+   integer, parameter :: NR = 3, NL = 25, NS = 6, NT = 20
+   integer :: i, j, k, fst, mdm, slw, idx, recno, eof
    character :: ch, flag
    character, dimension(25) :: buffer
    character (len = 6) :: sixes = 'NOKTYU'
-   character (len = 12) :: nstr
    character (len = 20) :: twenties = 'XEQLHBRMPDICJASVWGZF'
    character (len = 80) :: line
    integer, dimension(NS,NL) :: r6
    integer, dimension(NT,NL,NR) :: r20
    integer, dimension(0:3) :: levcnt
-   character, allocatable :: msg(:)
    ! data initializations
    data levcnt/4*1/
    ! processing
    call getarg(1, flag)
-   call getarg(2, nstr)
    if (flag /= 'e' .and. flag /= 'd') then
       write (*,*) 'Usage: purple e/d'
       stop 'processing terminated'
@@ -64,46 +61,45 @@ program purple
       mdm = 2
       slw = 1
    end if
-   read (nstr,*) n
    open (7,file='relays.dat',access='direct',form='formatted',recl=3300)
    read (7,1000,rec=recno) r6, r20
    close (7)
-   ! Read it all at once since Perl has filtered it already.
-   allocate (msg(n))
-   read (*,2000,iostat=eof) (msg(i),i=1,n)
-   i = 1
+   read (*,2000,iostat=eof) line
    j = 0
-   do i = 1, n
-      ch = msg(i)
-      ! to uppercase
-      if (ch >= 'a' .and. ch <= 'z') ch = char(ichar(ch) - 32)
-      if (ch < 'A' .or. ch > 'Z') cycle ! not alphabetic
-      ch = pegboard(ch, sixes, twenties, VOWELS, CONSONANTS)
-      idx = index(VOWELS, ch)
-      if (idx /= 0) then
-         j = j + 1
-         idx = relay_six(NS, NL, NR, r6, levcnt(0), idx)
-         buffer(j) = sixes(idx:idx)
-         call step(levcnt, fst, mdm, slw)
-         if (j == 25) then
-            write (*,3000) (buffer(idx), idx = 1, j)
-            j = 0
+   do while (eof == 0)
+      k = len(trim(line))
+      do i = 1, k
+         ch = line(i:i)
+         ! to uppercase
+         if (ch >= 'a' .and. ch <= 'z') ch = char(ichar(ch) - 32)
+         if (ch < 'A' .or. ch > 'Z') cycle ! not alphabetic
+         ch = pegboard(ch, sixes, twenties, VOWELS, CONSONANTS)
+         idx = index(VOWELS, ch)
+         if (idx /= 0) then
+            j = j + 1
+            idx = relay_six(NS, NL, NR, r6, levcnt(0), idx)
+            buffer(j) = sixes(idx:idx)
+            call step(levcnt, fst, mdm, slw)
+            if (j == 25) then
+               write (*,3000) (buffer(idx), idx = 1, j)
+               j = 0
+            end if
+            cycle ! we are done here
          end if
-         cycle ! we are done here
-      end if
-      idx = index(CONSONANTS, ch)
-      if (idx /= 0) then
-         j = j + 1
-         idx = relay_twenty(NT, NL, NR, r20, levcnt, idx)
-         buffer(j) = twenties(idx:idx)
-         call step(levcnt, fst, mdm, slw)
-         if (j == 25) then
-            write (*,3000) (buffer(idx), idx = 1, j)
-            j = 0
+         idx = index(CONSONANTS, ch)
+         if (idx /= 0) then
+            j = j + 1
+            idx = relay_twenty(NT, NL, NR, r20, levcnt, idx)
+            buffer(j) = twenties(idx:idx)
+            call step(levcnt, fst, mdm, slw)
+            if (j == 25) then
+               write (*,3000) (buffer(idx), idx = 1, j)
+               j = 0
+            end if
          end if
-      end if
+      end do
+      read (*,2000,iostat=eof) line
    end do
-   deallocate (msg)
    ! pad out final block of 5 if necessary
    do while (mod(j, 5) /= 0) 
       j = j + 1
@@ -112,8 +108,8 @@ program purple
    ! write final line if there is one
    if (j > 0) write (*,3000) (buffer(idx), idx = 1, j)
    1000 format (1650i2)
-   2000 format (262144a1)
-   3000 format (4(5a1,x),5a1)
+   2000 format (A)
+   3000 format (4(5A1,X),5A1)
 end program purple
 
 ! function emulates pegboard mapping of sixes and twenties
@@ -159,7 +155,7 @@ subroutine step(levcnt, fst, mdm, slw)
       
    levcnt(0) = levcnt(0) + 1  ! always advances
 
-   ! reset when advanced past 25th level
+   ! reset when on advance past 25th level
    do i = 0, 3
       if (levcnt(i) > 25) levcnt(i) = 1
    end do 
